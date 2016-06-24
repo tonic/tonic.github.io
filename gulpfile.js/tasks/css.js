96dbb49d712a63837b8ce37f3759ba483a4412e5
Taylor Smith <mail@taylorsmith.net>
2016-06-24 11:36:53 -0600

    Add webpack support for JS

--- gulpfile.js/tasks/css.js
+++ gulpfile.js/tasks/css.js
@@ -1,10 +1,12 @@
-var config = require('../config');
-var gulp = require('gulp');
 var autoprefixer = require('gulp-autoprefixer')
 var browserSync = require('browser-sync');
+var config = require('../config');
+var cssnano = require('gulp-cssnano')
+var gulp = require('gulp');
+var gulpif = require('gulp-if');
+var path = require('path');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
-var path = require('path');
 
 var paths = {
   src: path.join(config.root.src, config.tasks.css.src, '/**/*.{' + config.tasks.css.extensions + '}'),
@@ -13,11 +15,12 @@ var paths = {
 
 var cssTask = function () {
   return gulp.src(paths.src)
-    .pipe(sourcemaps.init())
+    .pipe(gulpif(!global.production, sourcemaps.init()))
     .pipe(sass(config.tasks.css.sass))
     .on('error', sass.logError)
     .pipe(autoprefixer(config.tasks.css.autoprefixer))
-    .pipe(sourcemaps.write())
+    .pipe(gulpif(global.production, cssnano({autoprefixer: false})))
+    .pipe(gulpif(!global.production, sourcemaps.write()))
     .pipe(gulp.dest(paths.dest))
     .pipe(browserSync.stream())
 }
