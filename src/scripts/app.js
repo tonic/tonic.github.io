0735c0f3a56b641585043a6f245a3defcc0b0aa0
Taylor Smith <mail@taylorsmith.net>
2016-07-29 16:12:41 -0600

    Enable contact form

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -2,3 +2,36 @@ import $ from 'jquery';
 import initMap from './map';
 
 google.maps.event.addDomListener(window, 'load', initMap);
+
+$(function() {
+  var $form = $('form#contact');
+
+  $form.submit(function(event) {
+    event.preventDefault();
+
+    var name    = $form.find('input[name="name"]').val();
+    var email   = $form.find('input[name="email"]').val();
+    var phone   = $form.find('input[name="phone"]').val();
+    var message = $form.find('textarea[name="message"]').val();
+
+    var data = {
+      lead: {
+        name: name,
+        email: email,
+        phone: phone,
+        message: message
+      }
+    };
+
+    $.ajax({
+      type: 'POST',
+      url: 'http://leads.tonic.io/api/leads',
+      data: data,
+      dataType: 'json',
+      encode: true
+    })
+    .done(function(data) {
+      console.log(data);
+    });
+  });
+});
