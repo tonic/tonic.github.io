e663798dcbe545a7e932fdddd0630f3c88de54f2
Taylor Smith <mail@taylorsmith.net>
2016-08-15 11:14:17 -0600

    Add Promise polyfill

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -3,6 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ContactForm from './components/ContactForm';
 import initMap from './map';
+import { Promise } from 'es6-promise';
 
 google.maps.event.addDomListener(window, 'load', initMap);
 
